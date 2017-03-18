434994ccfad2357cc98324f6efe97f5166f52b60
souravirus <souravsahoo66@yahoo.com>
2017-03-19 04:12:31 +0530

    Completed makeConfess.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/home.css">
+		<link rel="stylesheet" href="css/home.css?version=1">
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">    
 		<script> src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
@@ -24,7 +24,7 @@
 		<div id="mycreation">
             <img class="homeImg" src=images/confession.jpg>
             <a class="btn btn-lg btn-primary" id="see" href="#" role="button">See confessions</a>
-            <a class="btn btn-lg btn-primary" id="make" href="#" role="button">Make confession</a>
+            <a class="btn btn-lg btn-primary" id="make" href="makeConfess.php" role="button">Make confession</a>
 		</div>
 		
 	</body>
--- makeConfess.php
+++ makeConfess.php
@@ -1,9 +1,10 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-        <link rel="stylesheet" href="css/makeConfess.css">
+        <link rel="stylesheet" href="css/makeConfess.css?version=1">
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <title> Confession Page</title>
     </head>
     <body>
@@ -14,8 +15,38 @@
                     Required field
                 </strong>
             </div>
-            <form action="#" method="post">
-                
+            <form action="#" method="post" enctype="multipart/form-data">
+                    <div class="input">
+                        <input type="text" name="name" placeholder="Enter name">
+                        <span class="extra">
+                            <i class="fa fa-asterisk" aria-hidden="true"></i>  
+                        </span> 
+                    </div>
+                    <div class="input">
+                        <input type="text" name="Year" placeholder="Enter Year">
+                        <span class="extra">
+                            <i class="fa fa-asterisk" aria-hidden="true"></i>  
+                        </span> 
+                    </div>
+                    <div class="input">
+                        <input type="text" name="branch" placeholder="Enter branch">
+                        <span class="extra">
+                            <i class="fa fa-asterisk" aria-hidden="true"></i>  
+                        </span> 
+                    </div>
+                    <div class="input">
+                        <input type="text" name="email" placeholder="Enter email id">
+                        <span class="extra">
+                            <i class="fa fa-asterisk" aria-hidden="true"></i>  
+                        </span> 
+                    </div>
+                    <div class="input">
+                        <label for="fileToUpload"> 
+                            Please select a .txt file
+                        </label><br>
+                        <input type="file" name="fileToUpload"> 
+                    </div>
+                    <input type="submit" class="btn btn-info" id="submission" value="Submit" name="submit">
             </form>
         </div>
     </body>
