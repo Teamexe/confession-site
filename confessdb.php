8d0dac071618209c6033aea1bf11f24e1c323837
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-03 02:43:48 +0530

    updates

--- confessdb.php
+++ confessdb.php
@@ -2,43 +2,26 @@
 <html>
 <head>
     <title>Validate confession</title>
-    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
 <?php 
     
-
     $name="";
     $year="";
     $branch="";
     $email="";
     $message="";
   include_once 'includes/sql_config.php';
-    include_once 'secret.php';
   $db=mysqli_connect(HOST, USER, PASSWORD, DATABASE)
               or die('Error connecting to database');
     $email=mysqli_real_escape_string($db,$_POST["email"]);
     $email=test_input($email);
     $message=mysqli_real_escape_string($db,$_POST["confmsg"]);
     $message=test_input($message);
-    $captcha=$_POST["g-recaptcha-response"];
-    if(!$captcha){
-          echo '<h2>Please check the the captcha form.</h2>';
-          exit;
-        }
+    
     //$ip=$_SERVER['REMOTE_ADDR'];
     //myedits
-        echo $_SERVER['REMOTE_ADDR'];
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
-        $obj = json_decode($response);
-        if($obj->success == true)
-            {
-                    echo "Captcha is working";
-            }
-        else
-            {
-                    echo "Captcha is not working";
-            }
+        
     //edits end
 
     /*$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretKey.'&response='.$_POST['g-recaptcha-response']);
@@ -62,7 +45,8 @@
         $data = preg_replace( "/rn/", " ", $data );
         return $data;
     }
-    if($message ){
+    if($message)
+    {
         $sql="INSERT INTO adminperm (email, message, permission) values ('$email',' $message',1)";
         if(mysqli_query($db,$sql))
         {
--- verify.php
+++ verify.php
@@ -9,13 +9,53 @@
   if (!$resp->is_valid) 
   {
     // What happens when the CAPTCHA was entered incorrectly
-    echo "Captcha is wrong";
+    echo "Captcha entered is wrong<br>";
     
     die ("The reCAPTCHA wasn't entered correctly. Go back and try it again."."(reCAPTCHA said: ".$resp->error .")");
   } 
   else 
   {
     // Your code here to handle a successful verification
-    echo "captcha is valid";
+    include_once 'includes/sql_config.php';
+    $db=mysqli_connect(HOST, USER, PASSWORD, DATABASE)  or die('Error connecting to database');
+    $email=mysqli_real_escape_string($db,$_POST["email"]);
+    $email=test_input($email);
+    $message=mysqli_real_escape_string($db,$_POST["confmsg"]);
+    $message=test_input($message);
+
+    echo '<center><h2>Thanks for posting confession.</h2>';
+    echo '<a href="index.php">Check your confession here</a></center>';
+
+
+    function test_input($data) 
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        //fixed a bug which was cousing "rn" to appear whenever "enter" key is pressed
+        $data = preg_replace( "/rn/", " ", $data );
+        return $data;
+    }
+    if($message)
+    {
+        $sql="INSERT INTO adminperm (email, message, permission) values ('$email',' $message',1)";
+        if(mysqli_query($db,$sql))
+        {
+            echo "<center>Your confession has been posted</center>";
+            header( "refresh:5;url=index.php" );
+        }
+        
+        else
+        {
+            echo "<center>Sorry sending failed<br></center>";
+        }
+    }
+    else
+        {
+              echo "<center>You have to enter some message<br>
+              You're being redirected to previous page within 5 seconds<center>";
+              header( "refresh:5;url=makeConfess.php" );
+        }
+    mysqli_close($db);
   }
   ?>
