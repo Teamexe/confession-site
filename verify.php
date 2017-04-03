4eef0fefc1e4a36a74ca05faf564832977e12bf8
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-03 16:08:58 +0530

    new look

--- verify.php
+++ verify.php
@@ -1,3 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Confession site by - Team .EXE">
+    <meta name="author" content="Team .EXE">
+    <link rel="icon" href="exe.nith.ac.in/images/confess.png">
+
+    <title>Confession - Team .EXE</title>
+    <style type="text/css">
+.demo-card {
+  padding-top: 20px;
+  padding-left: 5%;
+  padding-right: 5%;
+  padding-bottom: 10px;
+}
+#return-to-top {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    background: rgb(0, 0, 0);
+    background: rgba(0, 0, 0, 0.7);
+    width: 50px;
+    height: 50px;
+    display: block;
+    text-decoration: none;
+    -webkit-border-radius: 35px;
+    -moz-border-radius: 35px;
+    border-radius: 35px;
+    display: none;
+    -webkit-transition: all 0.3s linear;
+    -moz-transition: all 0.3s ease;
+    -ms-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+}
+#return-to-top i {
+    color: #fff;
+    margin: 0;
+    position: relative;
+    left: 16px;
+    top: 13px;
+    font-size: 19px;
+    -webkit-transition: all 0.3s ease;
+    -moz-transition: all 0.3s ease;
+    -ms-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+}
+#return-to-top:hover {
+    background: rgba(0, 0, 0, 0.9);
+}
+#return-to-top:hover i {
+    color: #fff;
+    top: 5px;
+}
+    </style>
+    
+
+<?php 
+      include_once('stylesheets.php');
+      echo "</head>";
+?>
+<body>
+<?php
+      include_once('header.php');
+?> 
+<center>
+<div id="topp" class="page-header">
+<h1>NITH Confessions<small> - Team .EXE</small></h1>
+Confession Verification page
+</div>
 <?php
   require_once('recaptchalib.php');
   require_once('secret.php');
@@ -38,7 +112,8 @@
         if(mysqli_query($db,$sql))
         {
             echo '<center><h2>Thanks for posting confession.</h2>';
-            echo '<a href="index.php">Check your confession here</a></center>';
+            echo '<a href="index.php">Check your confession here</a><br>We are redirecting you in 6 seconds.</center>';
+            header( "refresh:6;url=index.php" );
         }
         
         else
@@ -56,3 +131,9 @@
 
       mysqli_close($db);
   ?>
+  </center>
+  <?php
+    include_once('footer.php');
+?>
+</body>
+</html>
