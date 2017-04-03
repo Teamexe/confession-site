d5b3085e508f4f59c21f5e8732add273caa07595
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-03 16:30:11 +0530

    footer modifications

--- footer.php
+++ footer.php
@@ -27,25 +27,5 @@
                 <div class="col-md-8">
                     <p class="copyright">&copy;: <span>2017</span> | Designed, Developed & Hosted by <b><a href="index.php" title="Team EXE official website">Team .EXE</a></b></p>
                 </div>
-                <div class="col-md-4">
-                    <!-- Social Media -->
-                    <ul class="social">
-                        <li>
-                            <a href="https://www.facebook.com/teamexe/" target="_blank" title="Like us on Facebook" class="Facebook">
-                                <i class="ion-social-facebook"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.youtube.com/channel/UCTIpvLaM1G-uUsthgCDauKw" target="_blank" title="Subscribe us on Youtube" class="Youtube">
-                                <i class="ion-social-youtube"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/teamexenith/" target="_blank" title="Follow on Instagram" class="Instagram">
-                                <i class="ion-social-instagram"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </footer> <!-- /#footer -->
