7d50f7bfae7996bf171c67bf149f1e6f8de463e9
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-03 15:44:06 +0530

    debugging

--- verify.php
+++ verify.php
@@ -1,6 +1,19 @@
 <?php
   require_once('recaptchalib.php');
   require_once('secret.php');
+  include_once ('includes/sql_config.php');
+  $db=mysqli_connect(HOST, USER, PASSWORD, DATABASE)  or die('Error connecting to database');
+
+  function test_input($data) 
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        //fixed a bug which was cousing "rn" to appear whenever "enter" key is pressed
+        $data = preg_replace( "/rn/", " ", $data );
+        return $data;
+    }
+
   $resp = recaptcha_check_answer ($privatekey,
                                 $_SERVER["REMOTE_ADDR"],
                                 $_POST["recaptcha_challenge_field"],
@@ -16,33 +29,16 @@
   else 
   {
     // Your code here to handle a successful verification
-    include_once 'includes/sql_config.php';
-    $db=mysqli_connect(HOST, USER, PASSWORD, DATABASE)  or die('Error connecting to database');
-    $email=mysqli_real_escape_string($db,$_POST["email"]);
-    $email=test_input($email);
     $message=mysqli_real_escape_string($db,$_POST["confmsg"]);
     $message=test_input($message);
 
-    echo '<center><h2>Thanks for posting confession.</h2>';
-    echo '<a href="index.php">Check your confession here</a></center>';
-
-
-    function test_input($data) 
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        //fixed a bug which was cousing "rn" to appear whenever "enter" key is pressed
-        $data = preg_replace( "/rn/", " ", $data );
-        return $data;
-    }
     if($message)
     {
-        $sql="INSERT INTO adminperm (email, message, permission) values ('$email',' $message',1)";
+        $sql="INSERT INTO adminperm (message, permission) values ('$message',1)";
         if(mysqli_query($db,$sql))
         {
-            echo "<center>Your confession has been posted</center>";
-            header( "refresh:5;url=index.php" );
+            echo '<center><h2>Thanks for posting confession.</h2>';
+            echo '<a href="index.php">Check your confession here</a></center>';
         }
         
         else
@@ -56,6 +52,7 @@
               You're being redirected to previous page within 5 seconds<center>";
               header( "refresh:5;url=makeConfess.php" );
         }
-    mysqli_close($db);
   }
+
+      mysqli_close($db);
   ?>
